2acdb6e7e2bb84dcf13a5e2a46650dfff038c01e
yoheikikuta <diracdiego@gmail.com>
2019-03-16 17:47:07 +0900

    Add option i and v to grep

--- grep.c
+++ grep.c
@@ -3,53 +3,91 @@
 #include <string.h>
 #include <sys/types.h>
 #include <regex.h>
+#include <unistd.h>
 
-static void do_grep(regex_t *pat, FILE *f);
+static void grep_file(regex_t *re, char *path);
+static void grep_stream(regex_t *re, FILE *f);
 
-int main(int argc, char *argv[]){
-    regex_t pat;
-    int err;
+static int opt_invert = 0;
+static int opt_ignorecase = 0;
 
-    if (argc < 2) {
+int main(int argc, char *argv[]) {
+    int opt;
+    while ((opt = getopt(argc, argv, "iv")) != -1) {
+        switch (opt) {
+        case 'i':
+            opt_ignorecase = 1;
+            break;
+        case 'v':
+            opt_invert = 1;
+            break;
+        case '?':
+            fprintf(stderr, "Usage: %s [-iv] [<file>...]\n", argv[0]);
+            exit(1);
+        }
+    }
+    // EX.) ./grep -i WHILE grep.c -> argc = 4, argv = ["./grep", "-i", "WHILE", "grep.c"]
+    //      optind = 2 : initialized as 1 and plus 1 for -i
+    //      argc -= optind -> argc = 2 
+    //      argv += optind -> ["WHILE", "grep.c"]
+    argc -= optind;
+    argv += optind;
+
+    if (argc < 1) {
         fputs("no pattern\n", stderr);
         exit(1);
     }
+    char *pattern = argv[0];
+    argc--;
+    argv++;
 
-    err = regcomp(&pat, argv[1], REG_EXTENDED | REG_NOSUB | REG_NEWLINE);
+    int re_mode = REG_EXTENDED | REG_NOSUB | REG_NEWLINE;
+    if (opt_ignorecase) re_mode |= REG_ICASE;
+    regex_t re;
+    int err = regcomp(&re, pattern, re_mode);
     if (err != 0) {
         char buf[1024];
 
-        regerror(err, &pat, buf, sizeof buf);
+        regerror(err, &re, buf, sizeof buf);
         puts(buf);
         exit(1);
     }
-
-    if (argc == 2) {
-        do_grep(&pat, stdin);
-    } else {
-        for (int i = 2; i < argc; i++) {
-            FILE *f;
-
-            f = fopen(argv[i], "r");
-            if (!f) {
-                perror(argv[i]);
-                exit(1);
-            }
-            do_grep(&pat, f);
-            fclose(f);
+    if (argc == 0) {
+        grep_stream(&re, stdin);
+    }
+    else {
+        int i;
+        for (i = 0; i < argc; i++) {
+            grep_file(&re, argv[i]);
         }
     }
-    regfree(&pat);
+    regfree(&re);
     exit(0);
 }
 
-static void do_grep(regex_t *pat, FILE *src) {
-    char buf[4096];
+static void grep_file(regex_t *re, char *path) {
+    FILE *f;
+    
+    f = fopen(path, "r");
+    if (!f) {
+        perror(path);
+        exit(1);
+    }
+    grep_stream(re, f);
+    fclose(f);
+}
 
-    while(fgets(buf, sizeof buf, src)) {
-        if (regexec(pat, buf, 0, NULL, 0) == 0) {
+static void grep_stream(regex_t *re, FILE *f) {
+    char buf[4096];
+    int matched;
+
+    while (fgets(buf, sizeof buf, f)) {
+        matched = (regexec(re, buf, 0, NULL, 0) == 0);
+        if (opt_invert) {
+            matched = !matched;
+        }
+        if (matched) {
             fputs(buf, stdout);
         }
     }
-    
 }
