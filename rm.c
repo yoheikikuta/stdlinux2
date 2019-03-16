ecb6e4e0e0dcad2928f268fc9e7e406914e764c4
yoheikikuta <diracdiego@gmail.com>
2019-03-17 00:49:05 +0900

    Add rm.c

---
+++ rm.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "%s: no arguments\n", argv[0]);
+        exit(1);
+    }
+    for (int i = 1; i < argc; i++) {
+        if (unlink(argv[i]) < 0) {
+            perror(argv[i]);
+            exit(1);
+        }
+    }
+    exit(0);
+}
